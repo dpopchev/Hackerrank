c1cd06485a470ac25d63f90ecf4b7b6d47dbef0e
dpopchev@gmail.com <dpopchev@gmail.com>
2020-08-28 14:02:30 +0300

    extend TC; trim leading and trailing empty spaces in match; less gredy regexp

--- Regex/Detect_HTML_links/solution.pl
+++ Regex/Detect_HTML_links/solution.pl
@@ -4,13 +4,13 @@ use warnings;
 use strict;
 use diagnostics;
 
-my $link_text = qr/<a\b[\w\s[:punct:]]+?\bhref="(?'LINK'[^"]*?)"[\w\s[:punct:]]*?>(?'TAGS'<\/?\w+>(?&TAGS)?)?(?'TEXT'[^<>\/]*)</;
+my $link_text = qr/<a\b[\w\s[:punct:]]+?\bhref="(?'LINK'[^"]*?)"[\w\s[:punct:]]*?>(?'TAGS'<\/?[\w\s[:punct:]]+?>(?&TAGS)?)?(?'TEXT'[\w\s[:punct:]]*?)(?=<\/\w+>)/;
 
 while (<>){
     while ($_ =~ m/$link_text/g) {
-        print defined $+{LINK} ? $+{LINK} : '',
+        print defined $+{LINK} ? $+{LINK} =~ s/^\s+|\s+$//r : '',
               ",",
-              defined $+{TEXT} ? $+{TEXT} : '',
+              defined $+{TEXT} ? $+{TEXT} =~ s/^\s+|\s+$//r : '',
               "\n";
     }
 }
