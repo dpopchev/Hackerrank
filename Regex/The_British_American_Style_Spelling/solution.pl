601e307ebe2cb6c0d865581d62fbdb9b3fccf0ba
dpopchev@gmail.com <dpopchev@gmail.com>
2020-08-30 17:27:55 +0300

    init commit on british american style spelling problem

---
+++ Regex/The_British_American_Style_Spelling/solution.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/env perl
+
+use warnings;
+use strict;
+use diagnostics;
+
+use Data::Dumper;
+
+# lets try to match searched words,
+# may fail if text also contains single word on a line
+my $word_tc = qr/(?'W_TC'[a-zA-Z]+)(ze|se)$/;
+
+# slurp test case text
+my $text;
+
+while (my $input_line = <>){
+    if ($input_line =~ /$word_tc/){
+        my $word_two_forms = qr/\b$+{W_TC}(?:ze|se)\b/;
+        my $count_matches = () = $text =~ /$word_two_forms/gi;
+
+        print $count_matches,"\n";
+    }else {
+        $text .= $input_line;
+    }
+}
