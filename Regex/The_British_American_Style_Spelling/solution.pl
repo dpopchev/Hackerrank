cced98890bb2f9df1699021a8cf18130bd01336c
dpopchev@gmail.com <dpopchev@gmail.com>
2020-08-31 08:17:01 +0300

    extend solution regexp to match inner word; used in UK and US Part 2

--- Regex/The_British_American_Style_Spelling/solution.pl
+++ Regex/The_British_American_Style_Spelling/solution.pl
@@ -8,14 +8,14 @@ use Data::Dumper;
 
 # lets try to match searched words,
 # may fail if text also contains single word on a line
-my $word_tc = qr/(?'W_TC'^[a-zA-Z]+)(ze|se)$/;
+my $word_tc = qr/(?'W_TC_Leading'^[a-z]*)(ze|se)(?'W_TC_Trailling'[a-z]*)$/;
 
 # slurp test case text
 my $text;
 
 while (my $input_line = <>){
     if ($input_line =~ /$word_tc/){
-        my $word_two_forms = qr/\b$+{W_TC}(?:ze|se)\b/;
+        my $word_two_forms = qr/\b$+{W_TC_Leading}(?:ze|se)$+{W_TC_Trailling}\b/;
 
         my @matches = $text =~ /$word_two_forms/gi;
 
